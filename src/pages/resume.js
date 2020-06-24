604657fc22e18fefcae0f021bef949a6e1dd61a3
Tamim <TamimChowdhury@live.com>
2020-06-24 03:28:13 -0400

    Resume Page
    In development

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
+import {Container, Col, Row, Jumbotron} from 'react-bootstrap'
 
 class Resume extends Component {
     render(){
         return(
-            <div>
-                <h1> Resume Page </h1>
+            <div className = "cloud">
+                <Container fluid = "true" className = "flex-container">
+                    <Jumbotron className = "resumebox-left" />
+                    <Jumbotron className = "resumebox-right"/>
+                </Container>
             </div>
         );
     }
